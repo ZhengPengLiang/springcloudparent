336c26927d75e65d99c8e786e4ebb4d76c01f6a4
zhengpengliang <zhengpengliang@100tal.com>
2020-06-03 14:12:24 +0800

    project start

---
+++ nacos-customer/src/main/java/com/nacos/nacoscustomer/NacosCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.nacos.nacoscustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosCustomerApplication.class, args);
+    }
+
+}
---
+++ nacos-customer/src/test/java/com/nacos/nacoscustomer/NacosCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nacos.nacoscustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
